e42f93a13620a62eb4d7e033278e5a51003254ca
edinafazakas <fazakas_edina@yahoo.ro>
2024-09-25 16:25:25 +0300

    creating tasks, complete tasks

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,21 +1,23 @@
-import { Component, computed, Input, input} from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
 
-  get imagePath(){
-    return 'assets/users/' + this.avatar
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser(){
-    //this.avatar.set();
+  onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
