b6026fe3e0a8edbd8cc7221d030e5f3e8c41a395
edinafazakas <fazakas_edina@yahoo.ro>
2024-09-30 18:21:13 +0300

    adding signal

--- investments-app/src/app/app.component.ts
+++ investments-app/src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { type InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  resultsData = signal<{
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[] | undefined>(undefined);
+
   onCalculateInvestmentResults(data: InvestmentInput) {
 
     const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
@@ -32,6 +42,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData.set(annualData);
   }
 }
