15bf07f8633ef42850cd551446a21efe7893c1e7
edinafazakas <fazakas_edina@yahoo.ro>
2024-10-15 01:37:20 +0300

    injecting values

--- service-deep-dive-project/src/app/tasks/tasks-list/tasks-list.component.ts
+++ service-deep-dive-project/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -3,6 +3,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
 import { TaskServiceToken } from '../../../main';
+import { TASK_STATUS_OPTIONS, taskStatusOptionProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,10 +11,13 @@ import { TaskServiceToken } from '../../../main';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [taskStatusOptionProvider] 
 })
 export class TasksListComponent {
   private selectedFilter = signal<string>('all');
   private tasksService = inject(TaskServiceToken);
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
+
   tasks = computed(() => {
     switch (this.selectedFilter()) {
       case 'all':
--- service-deep-dive-project/src/main.ts
+++ service-deep-dive-project/src/main.ts
@@ -3,9 +3,11 @@ import { AppComponent } from './app/app.component';
 import { TasksService } from './app/tasks/tasks.service';
 import { InjectionToken } from '@angular/core';
 
-export const TaskServiceToken = new InjectionToken<TasksService>('task-service-tokeen')
+export const TaskServiceToken = new InjectionToken<TasksService>(
+  'task-service-token'
+);
 bootstrapApplication(AppComponent, {
-    providers: [{provide: TaskServiceToken, useClass: TasksService}],
+  providers: [{ provide: TaskServiceToken, useClass: TasksService }],
 }).catch((err) => console.error(err));
 
 // bootstrapApplication(AppComponent).catch((err) => console.error(err));
