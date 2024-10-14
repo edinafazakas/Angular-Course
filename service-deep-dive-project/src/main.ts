f68503189ca247403d8045ed428b8a1689c24b8a
edinafazakas <fazakas_edina@yahoo.ro>
2024-10-15 01:16:38 +0300

    adding custom tokens for service

--- service-deep-dive-project/src/main.ts
+++ service-deep-dive-project/src/main.ts
@@ -1,8 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { TasksService } from './app/tasks/tasks.service';
+import { InjectionToken } from '@angular/core';
 
-// bootstrapApplication(AppComponent, {
-//     providers: [TasksService]
-// }).catch((err) => console.error(err));
+export const TaskServiceToken = new InjectionToken<TasksService>('task-service-tokeen')
+bootstrapApplication(AppComponent, {
+    providers: [{provide: TaskServiceToken, useClass: TasksService}],
+}).catch((err) => console.error(err));
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
